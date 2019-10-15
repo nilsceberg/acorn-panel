b07b5f69e4f52fdf4c8eaba82f08adb610b7a1d5
Nils Ceberg <nils.ceberg@gmail.com>
2019-10-15 15:09:16 +0200

    Add Material UI

--- fuse.js
+++ fuse.js
@@ -6,6 +6,7 @@ const fuse = FuseBox.init({
 	homeDir: "src",
 	output: "dist/$name.js",
 	target: "browser",
+	experimentalFeatures: true,
 //	plugins: [
 //		[SassPlugin(), CSSPlugin()]
 //	],
@@ -16,7 +17,7 @@ fuse
 	.bundle("app")
 	.instructions(`> index.tsx`)
 	.watch()
-	.hmr();
+	.hmr({ socketURI: "ws://localhost:4445" });
 
 fuse.dev({ root: false, port: 4445 }, server => {
 	const dist = path.resolve("./dist");
--- src/view/App.tsx
+++ src/view/App.tsx
@@ -2,13 +2,36 @@ import * as React from "react";
 import { observer } from "mobx-react";
 import { Model } from "../model/Model";
 
+import Button from "@material-ui/core/Button";
+import { CssBaseline } from "@material-ui/core";
+import { makeStyles, withStyles } from "@material-ui/styles";
+import { Classes } from "@material-ui/styles/mergeClasses/mergeClasses";
+import * as PropTypes from "prop-types";
+
 interface AppProps {
 	model: Model;
+	classes: Classes;
 }
 
-@observer
-export class App extends React.Component<AppProps> {
-	render() {
-		return <div>{this.props.model.systemMessage}</div>;
+const styles = (theme: any) => ({
+	root: {
+		display: "flex",
+		backgroundColor: "green",
 	}
-}
+})
+
+const useStyles = makeStyles(styles);
+
+export const App = withStyles(styles)(observer(
+	class extends React.Component<AppProps> {
+		render() {
+			return (
+				<div className={this.props.classes.root}>
+					<CssBaseline/>
+					{this.props.model.systemMessage}
+					<Button onClick={() => this.props.model.systemMessage = "CLICKED!" } variant="contained" color="primary">Log In</Button>
+				</div>
+			);
+		}
+	}
+));
